5cc10ec1cc766a8851d755a26f97d0a4735500b4
Andrew Liebchen <andrewliebchen@gmail.com>
2019-02-17 15:40:50 -0800

    Download csv

--- src/Sample.js
+++ src/Sample.js
@@ -1,34 +1,45 @@
 import React from "react";
-import { Box, Heading, Text, Flex } from "rebass";
+import { Box, Heading, Text, Flex, Button } from "rebass";
 import store from "./store";
 import { view } from "react-easy-state";
 import SampleRow from "./SampleRow";
 import CountControl from "./CountControl";
 import { getSamples } from "./utils";
+import CsvDownloader from "react-csv-downloader";
+import datef from "datef";
 
-const Sample = props => (
-  <Box>
-    <Flex>
-      <Heading>Sample</Heading>
-      <CountControl />
-    </Flex>
-    <table>
-      <thead>
-        <tr>
-          {store.elements.map((element, i) => (
-            <th key={i}>
-              <Text>{element}</Text>
-            </th>
+const Sample = props => {
+  const samples = getSamples(store.count, store.elements);
+  return (
+    <Box>
+      <Flex>
+        <Heading>Sample</Heading>
+        <CountControl />
+        <CsvDownloader
+          datas={samples}
+          filename={`Sample ${datef("MM-dd-YY h:mm:ss", new Date())}`}
+        >
+          <Button>CSV</Button>
+        </CsvDownloader>
+      </Flex>
+      <table>
+        <thead>
+          <tr>
+            {store.elements.map((element, i) => (
+              <th key={i}>
+                <Text>{element}</Text>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {samples.map((sample, i) => (
+            <SampleRow key={i} sample={sample} />
           ))}
-        </tr>
-      </thead>
-      <tbody>
-        {getSamples(store.count, store.elements).map((sample, i) => (
-          <SampleRow key={i} sample={sample} />
-        ))}
-      </tbody>
-    </table>
-  </Box>
-);
+        </tbody>
+      </table>
+    </Box>
+  );
+};
 
 export default view(Sample);
